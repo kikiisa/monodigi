915f0cdfb76ac27fe1d766eb6aab1e7155b85218
kikiisa <kikiisa89@gmail.com>
2023-04-02 22:39:46 +0700

    oke

--- public/broadcast.js
+++ public/broadcast.js
@@ -1,20 +1,13 @@
 let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-
 if( theme == 'dark' ){
     document.documentElement.classList.add('dark')
 }
-
 document.getElementById('dark-mode-toggle').addEventListener('click', function(){
    document.documentElement.classList.toggle('dark')
 });
-
-
-
 document.getElementById('song-saved').addEventListener('click', function(){
 	document.getElementById('song-saved').classList.toggle('saved');
 });
-
-
 Amplitude.init({
     "bindings": {
         37: 'prev',
@@ -28,7 +21,6 @@ Amplitude.init({
             if( isNaN( percentage ) ){
                 percentage = 0;
             }
-
             /**
              * Massive Help from: https://nikitahl.com/style-range-input-css
              */
@@ -38,15 +30,14 @@ Amplitude.init({
     },
     "songs": [
 		{
-			"name": "Silam",
-			"artist": "Heroin",
-			"album": "Siaran Minggun Malam",
+			"name": "offline",
+			"artist": "offline",
+			"album": "offline",
 			"url": "https://4a79-114-125-170-194.ap.ngrok.io/stream",
 			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
 		},
     ]
 });
-
 window.onkeydown = function(e) {
     return !(e.keyCode == 32);
 };
